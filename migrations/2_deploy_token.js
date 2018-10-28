a860b6e9431ac8c05f91800799cbd3c088eb7d6f
David, Shin <dirmich@gmail.com>
2018-10-29 00:54:07 +0900

    change sample token

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,5 +1,9 @@
 const token = artifacts.require('./SampleToken.sol')
 
 module.exports = async deployer => {
-  await deployer.deploy(token)
+  const name = "Sample Nuxtjs Token";
+  const symbol = "SNUT";
+  const decimals = 18;
+  const totalSupply = 10000;
+  await deployer.deploy(token, name, symbol, decimals, totalSupply)
 }
